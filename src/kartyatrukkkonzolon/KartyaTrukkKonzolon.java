a508d1f35f09f98c3949eb575afbcd7f368e7339
CsoltiP <csolti@szamalk-szalezi.hu>
2023-09-15 18:21:27 +0200

    kirak(int) algoritmusa

--- src/kartyatrukkkonzolon/KartyaTrukkKonzolon.java
+++ src/kartyatrukkkonzolon/KartyaTrukkKonzolon.java
@@ -6,7 +6,7 @@ public class KartyaTrukkKonzolon {
 
     private static String[] pakli = new String[22];
     private static final Scanner sc = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
         feltolt();
         for (int i = 0; i < 3; i++) {
@@ -26,12 +26,12 @@ public class KartyaTrukkKonzolon {
                 pakli[i++] = szin + "_" + ertekek[e];
             }
         }
-        
+
     }
     
     private static void kirak() {
         for (int i = 1; i < pakli.length; i++) {
-            System.out.printf("%-8s",pakli[i]);
+            System.out.printf("%-8s", pakli[i]);
             if (i % 3 == 0) {
                 System.out.println("");
             }
@@ -41,16 +41,41 @@ public class KartyaTrukkKonzolon {
     private static int melyik() {
         boolean jo;
         int oszlop;
-        do{
+        do {
             System.out.print("melyik oszlop (1-3): ");
             oszlop = sc.nextInt();
             jo = oszlop >= 1 && oszlop <= 3;
-        }while(!jo);
+        } while (!jo);
         return oszlop;
     }
 
     private static void kever(int oszlop) {
         // mindig középre a választott
+        String[] ujPakli = new String[22];
+        switch (oszlop) {
+            case 1:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[20 - (i - 1) * 3];
+                }
+                break;
+        }
+        pakli = ujPakli;
     }
 
     private static void ezVolt() {
